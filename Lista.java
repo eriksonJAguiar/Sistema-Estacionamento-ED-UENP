3be670220ec97ae2a41604f4466971455f5f897b
Erikson Aguiar <erjulioaguiar@gmail.com>
2015-09-14 23:35:39 -0300

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -1,40 +1,124 @@
 package estruturas;
 
-import cadastro.Carro;
+import cadastro.Cliente;
 
 
-public class Pilha {
+
+
+public class Lista{
     public static class No{
-        Carro item;
+        Cliente item;
         No prox;
     }
     
-    private No topo;
+    private No primeiro;
+    private No ultimo;
     private int tamanho;
     
-    public Pilha(){
-        topo = null;
+    public Lista(){
+        primeiro = new No();
+        primeiro.prox = null;
+        ultimo = primeiro;
         tamanho = 0;
     }
-    
-    public int getTamanho(){
-        return tamanho;
-    }
-    
-    public void empilhar(Carro t){
-        No aux = new No();
-        aux.item = t;
-        aux.prox = topo;
-        topo = aux;
+    public void inserirUltimo(Cliente cliente){
+        ultimo.prox = new No();
+        ultimo = this.ultimo.prox;
+        ultimo.item = cliente;
+        ultimo.prox = null;
         tamanho++;
+    }
+    public void inserirPrimeiro(Cliente cliente){
+        if(tamanho == 0){
+             No novo = new No();
+            novo.item = cliente;
+            primeiro.prox = novo;
+            ultimo = novo;
+            ultimo.prox = null;
+            tamanho++;
+        }
+        else{
+            No novo = new No();
+            novo.item = cliente;
+            novo.prox = primeiro.prox;
+            primeiro.prox = novo;
+            tamanho++;
+        }
+       
         
     }
-    public Carro desempilha()throws Exception{
-        if(topo == null) 
-          throw new Exception("ERRO: a pilha está vazia");
-        No aux = topo;
-        topo = topo.prox;
-        tamanho--;
-        return (Carro) aux.item;
+    
+    public void inserirOrdenado(Cliente cliente){
+        if(primeiro == ultimo){
+            inserirPrimeiro(cliente);
+            return;
+        }
+        else{
+            No aux = primeiro;
+            No aux2 = primeiro;
+             int val = 0;
+            while(aux.prox != null && val <= 0){
+                
+               val = aux.prox.item.getNome().compareToIgnoreCase(cliente.getNome());
+               aux2 = aux;
+                aux  = aux.prox;
+                
+            }
+             if(val > 0){
+               No novo = new No();
+               novo.item = cliente;
+               novo.prox = aux;
+               aux2.prox = novo;
+               tamanho++;
+             }
+             else{
+                 inserirUltimo(cliente);
+             }
+        }
+        
+    }
+    public Cliente remover(long cpf){
+        No aux = primeiro;
+        int pos = 0;
+        while(aux.prox != null && aux.prox.item.getCpf() != cpf){
+                 pos++;
+                 aux = aux.prox;
+        }
+         if(pos == 0){
+                No dado = primeiro.prox;
+                aux = primeiro.prox;
+                primeiro.prox = primeiro.prox.prox;
+                aux.prox = null;
+                 tamanho--;
+            
+                return dado.item;   
+                }
+                else if(aux.prox == null){
+                Cliente dado = ultimo.item;
+                ultimo = aux;
+                aux.prox = null;
+                ultimo.prox = null;
+                tamanho--;
+                return dado;
+                }
+                else{
+                No aux2 = aux.prox;
+                aux.prox = aux.prox.prox;
+                Cliente dado = aux2.item;
+                aux2.prox = null;
+                tamanho--;
+                return dado;
+                }
+       
+    }
+    public void imprime(){
+        No aux = primeiro.prox;
+        while(aux != null){
+            System.out.println(aux.item.getNome());
+            aux = aux.prox;
+        }
+    }
+    public int getTamanho(){
+        return tamanho;
     }
 }

da97f3ec3bafccd5f1273e9c2d1e9f61ddb0f31a
Erikson Aguiar <erjulioaguiar@gmail.com>
2015-09-17 23:53:45 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,40 +1,46 @@
 package cadastro;
 
 
+
+
 public class Cliente{
     
     private String nome;
     private long cpf;
-    private Carro carro;
+   
     
     public Cliente(){
         nome = "";
         cpf = 0;
-        carro = new Carro();
        
     }
+    /**
+     * 
+     * @return String - nome do cliente
+     */
     public String getNome() {
         return nome;
     }
-
+    /**
+     * 
+     * @param nome - isere nome cliente
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+    /**
+     * 
+     * @return long - cpf cliente
+     */
     public long getCpf() {
         return cpf;
     }
-
+    /**
+     * 
+     * @param cpf - insere cpf
+     */
     public void setCpf(long cpf) {
         this.cpf = cpf;
-    }
-
-    public Carro getCarro() {
-        return carro;
-    }
-
-    public void setCarro(Carro carro) {
-        this.carro = carro;
     }
     
 }
